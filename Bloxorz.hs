60834f68d0ef5c6fdb80b412a1e52c24ce450875
madalina.boboc <madalina.boboc@stud.acs.upb.ro>
2019-05-04 16:39:30 +0300

    35/100

--- Bloxorz.hs
+++ Bloxorz.hs
@@ -37,18 +37,18 @@ type Position = (Int, Int)
 data Directions = North | South | West | East
     deriving (Show, Eq, Ord)
 
-data Cell = H | S | B | Sw | E | W
+data Cell = H | S | B | Sw [Position] | E | W
  deriving (Eq, Ord)
 
 instance Show Cell where
  show H = charToString hardTile
  show S = charToString softTile
  show B = charToString block
- show Sw = charToString switch
+ show (Sw a) = charToString switch
  show E = charToString emptySpace
  show W = charToString winningTile
 
-data Level = Level Position (Position, Position) (A.Array Position Cell) (A.Array Position [Position])
+data Level = Level Bool Position (Position, Position) (A.Array Position Cell)
     deriving (Eq, Ord)
 
 {-
@@ -65,51 +65,37 @@ data Level = Level Position (Position, Position) (A.Array Position Cell) (A.Arra
 -- instance Ord Level where
 --     compare = undefined
 
---addBlock :: Level -> Level
---addBlock (Level posColt (y,x) arr arrS) = Level posColt (y,x) (arr A.// [((x,y), B)]) arrS
-
---addBlock :: Position->Position->A.Array Position Cell->A.Array Position [Position]
---addBlock posColt (x1,y1) arr arrS = Level posColt (x1, y1) arr 
-
 addBlock :: (Position, Position)->A.Array Position Cell->A.Array Position Cell
 addBlock (p1, p2) arr
                    | p1 == p2 = arr A.// [(p1, B)]
                    | otherwise = (arr A.// [(p1, B)]) A.// [(p2, B)]
 instance Show Level where
- show (Level (x,y) posBlock arr arrS) = "\n" ++ m where
-  m = unlines [B.concat [show ((addBlock posBlock arr) A.! (i, j)) | i <- [0..x]] | j <- [0..y]]
+ show (Level start (x,y) (p1,p2) arr) = "\n" ++ m where
+  m = unlines [B.concat [show ((addBlock (p1,p2) arr) A.! (i, j)) | i <- [0..x]] | j <- [0..y]] ++ message where message | continueGame (Level start (x,y) (p1,p2) arr) == False && start == True = "Game Over\n"
+                                                                                                                         | p1 == p2 && (arr A.! p1) == W = "Congrats! You won!\n"
+                                                                                                                         | otherwise = ""
 
 makeMap :: Position->A.Array Position Cell
 makeMap (x,y) = a where
  a = A.array ((0,0),(x,y)) [((i,j), E) | i <- [0..x], j <- [0..y]]
 
-makeArrS :: Position->A.Array Position [Position]
-makeArrS (x,y) = a where
- a = A.array ((0,0), (x,y)) [((i,j), []) | i <- [0..x], j <- [0..y]]
-
 emptyLevel :: Position -> Position -> Level
-emptyLevel (x,y) (y1,x1) = Level (y,x) ((x1, y1), (x1, y1)) (makeMap (y,x)) (makeArrS (y,x))
+emptyLevel (x,y) (y1,x1) = Level False (y,x) ((x1, y1), (x1, y1)) (makeMap (y,x))
 
 addTile :: Char -> Position -> Level -> Level
-addTile t (y,x) (Level posColt posBlock arr arrS) = Level posColt posBlock (arr A.// [((x,y), c)]) arrS where c | t == 'S' = S 
-                                                                                                                | t == 'H' = H
-                                                                                                                | t == 'W' = W
+addTile t (y,x) (Level start posColt posBlock arr) = Level False posColt posBlock (arr A.// [((x,y), c)]) where c   | t == 'S' = S 
+                                                                                                                    | t == 'H' = H
+                                                                                                                    | t == 'W' = W
 addSwitch :: Position -> [Position] -> Level -> Level
-addSwitch (y,x) lst (Level posColt posBlock arr arrS) = Level posColt posBlock (arr A.// [((x,y), Sw)]) (arrS A.// [((x,y), lst)])
+addSwitch (y,x) lst (Level start posColt posBlock arr) = Level False posColt posBlock (arr A.// [((x,y), (Sw lst))])
 
-{-
-    === MOVEMENT ===
--}
-
-{-
-    *** TODO ***
-    Activate va verifica dacă mutarea blocului va activa o mecanică specifică. 
-    În funcție de mecanica activată, vor avea loc modificări pe hartă. 
--}
+inv :: Position -> Position
+inv (x,y) = (y,x)
 
 activate :: Cell -> Level -> Level
-activate = undefined
-
+activate (Sw a) (Level start posColt posBlock arr) | (arr A.! (inv (a !! 0))) == E = Level start posColt posBlock (foldl (\x (x1, y1) -> x A.// [((y1,x1), H)]) arr a)
+                                                   | otherwise = Level start posColt posBlock (foldl (\x (x1,y1) -> x A.// [((y1,x1), E)]) arr a)
+activate _ lvl = lvl
 {-
     *** TODO ***
     Mișcarea blocului în una din cele 4 direcții 
@@ -117,7 +103,25 @@ activate = undefined
 -}
 
 move :: Directions -> Level -> Level
-move = undefined
+move South (Level start posColt ((x,y), (x1,y1)) arr)   | x == x1 && y == y1 = activate (arr A.! (x1, y1 + 2)) (activate (arr A.! (x,y + 1)) (Level True posColt ((x,y + 1), (x1, y1 + 2)) arr))
+                                                        | otherwise = lvl where 
+                                                            lvl | x == x1 = activate (arr A.! (x1, y1 + 1)) (Level True posColt ((x1,y1 + 1), (x1, y1 + 1)) arr)
+                                                                | y == y1 = activate (arr A.! (x1, y1 + 1)) (activate (arr A.! (x,y + 1)) (Level True posColt ((x,y + 1), (x1, y1 + 1)) arr))
+
+move North (Level start posColt ((x,y), (x1,y1)) arr)   | x == x1 && y == y1 = activate (arr A.! (x1, y1 - 2)) (activate (arr A.! (x,y - 1)) (Level True posColt ((x,y - 1), (x1, y1 - 2)) arr))
+                                                        | otherwise = lvl where
+                                                            lvl | x == x1 = activate (arr A.! (x, y - 1)) (Level True posColt ((x,y - 1), (x, y - 1)) arr)
+                                                                | y == y1 = activate (arr A.! (x1, y1 - 1)) (activate (arr A.! (x,y - 1)) (Level True posColt ((x,y - 1), (x1, y1 - 1)) arr))
+
+move West (Level start posColt ((x,y), (x1,y1)) arr)    | x == x1 && y == y1 = activate (arr A.! (x1 - 2, y1)) (activate (arr A.! (x - 1, y)) (Level True posColt ((x - 2,y), (x1 - 1, y1)) arr))
+                                                        | otherwise = lvl where
+                                                            lvl | x == x1 = activate (arr A.! (x1 - 1, y1)) (activate (arr A.! (x - 1, y)) (Level True posColt ((x - 1,y), (x1 - 1, y1)) arr))
+                                                                | y == y1 = activate (arr A.! (x - 1, y)) (Level True posColt ((x - 1,y), (x - 1, y)) arr)
+
+move East (Level start posColt ((x,y), (x1,y1)) arr)    | x == x1 && y == y1 = activate (arr A.! (x1 + 2, y1)) (activate (arr A.! (x + 1, y)) (Level True posColt ((x + 1,y), (x1 + 2, y1)) arr))
+                                                        | otherwise = lvl where
+                                                            lvl | x == x1 = activate (arr A.! (x1 + 1, y1)) (activate (arr A.! (x + 1, y)) (Level True posColt ((x + 1,y), (x1 + 1, y1)) arr))
+                                                                | y == y1 = activate (arr A.! (x1 + 1, y1)) (Level True posColt ((x1 + 1, y1), (x1 + 1, y1)) arr)
 
 {-
     *** TODO ***
@@ -126,7 +130,9 @@ move = undefined
 -}
 
 continueGame :: Level -> Bool
-continueGame = undefined
+continueGame (Level start posColt (p1, p2) arr) | p1 == p2 && (arr A.! p1) == S = False
+                                          | (arr A.! p1) == E || (arr A.! p2) == E = False
+                                          | otherwise = True 
 
 {-
     *** TODO ***
